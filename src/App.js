e09cddfc293a4fe572cdaac3952f03ba7115e111
Rai <shubham.rai@siemens.com>
2019-08-05 09:46:26 +0530

    chnaged message

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
           <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
-          I am testing CI and CD
+          I am testing CI and CD in feature branch
         </p>
       </div>
     );
